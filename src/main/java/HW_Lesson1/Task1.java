79ff0151f9de251d7c0a4ed3e88f285e44cbbfa4
Polina <ms.maylin@inbox.ru>
2023-05-31 15:27:11 +0300

    In HW_Lesson1: Task1 changed

--- src/main/java/HW_Lesson1/Task1.java
+++ src/main/java/HW_Lesson1/Task1.java
@@ -4,11 +4,21 @@ package HW_Lesson1;
 import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Введите n треугольного числа: ");
+        System.out.print("Введите n треугольного числа: ");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
+        // Вычисление треугольного числа
         int result = number * (number+1)/2;
         System.out.println(number + " по счету треугольное число: " + result);
+        System.out.println();
+        // Вычисление факториала
+        System.out.print("Введите число для вычисления факториала: ");
+        number = scanner.nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= number ; i++) {
+            factorial *= i;
+        }
+        System.out.println("Факториал числа " + number + " = " + factorial);
         scanner.close();
     }
 
