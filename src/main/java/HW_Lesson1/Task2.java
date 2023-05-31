1f6c15dadc2aad3869680021ab9c9bbc174f1767
Polina <ms.maylin@inbox.ru>
2023-05-31 13:56:33 +0300

    In HW_Lesson1: Task2 added

---
+++ src/main/java/HW_Lesson1/Task2.java
@@ -0,0 +1,11 @@
+//2) Вывести все простые числа от 1 до 1000
+
+package HW_Lesson1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 1001; i++) {
+            System.out.println(i);
+        }
+    }
+}
