7e2042f8d0293a08fec5f084907742f94f1e6ca1
Polina <ms.maylin@inbox.ru>
2023-05-31 13:53:30 +0300

    In HW_Lesson1: Task1 added

--- src/main/java/HW_Lesson1/Task1.java
+++ src/main/java/HW_Lesson1/Task1.java
@@ -1,1 +1,15 @@
+//1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
 
+package HW_Lesson1;
+import java.util.Scanner;
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Введите n треугольного числа: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int result = number * (number+1)/2;
+        System.out.println(number + " по счету треугольное число: " + result);
+        scanner.close();
+    }
+
+}
